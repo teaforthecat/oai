24bd9aad34b8d7081f0d4074037b65acd0a35b4e
Chris Thompson <chris.thompson@walkerart.org>
2012-04-11 15:25:39 -0500

    as_json on CdwaLite

--- lib/oai/client/cdwa_lite.rb
+++ lib/oai/client/cdwa_lite.rb
@@ -1,34 +1,40 @@
 class CdwaLite
-  include OAI::XPath
-  attr_accessor :title, :creator, :date, :materials, :description, 
-  :classification, :image, :related, :url
+  attr_accessor :element
+  attr_accessor :title, :creator, :date, :materials, :description,
+                :subject, :classification, :image, :related, :url
 
   def initialize(element)
-    debugger
-    @title = xpath(element,
-                   'cdwalite:cdwalite/cdwalite:titleWrap/cdwalite:titleSet/cdwalite:title[@cdwalite:pref="preferred"]/text()')
-    @creator = xpath(element, 
-                     'cdwalite:cdwalite/cdwalite:displayCreator/text()')
-    @date = xpath(element, 
-                  'cdwalite:cdwalite/cdwalite:indexingDatesWrap/cdwalite:indexingDatesSet/cdwalite:earliestDate/text()')
-    @materials = xpath(element, 
-                       'cdwalite:cdwalite/cdwalite:displayMaterialsTech/text()')
-    @description = xpath(element, 
-                         'cdwalite:cdwalite/cdwalite:descriptiveNoteWrap/cdwalite:descriptiveNoteSet/cdwalite:descriptiveNote/text()')
-    @subject = xpath(element, 
-                     'cdwalite:cdwalite/cdwalite:indexingSubjectWrap/cdwalite:indexingSubjectSet/cdwalite:subjectTerm[@cdwalite:termsource="WAC"]/text()')
-    @classification = xpath(element, 
-                            'cdwalite:cdwalite/cdwalite:classificationWrap/cdwalite:classification[@cdwalite:termsource="WAC"]/text()')
-    @image = xpath(element,
-                   'cdwalite:cdwalite/cdwalite:resourceWrap/cdwalite:resourceSet/cdwalite:resourceType[contains(text(), "Digital Image")]/../cdwalite:linkResource/text()')
-    @related = xpath(element, 
-                     'cdwalite:cdwalite/cdwalite:resourceWrap/cdwalite:resourceSet/cdwalite:resourceType[contains(text(), "text")]/../cdwalite:resourceID/text()')
-    @url = xpath(element, 
-                 'cdwalite:cdwalite/cdwalite:recordWrap/cdwalite:recordInfoWrap/cdwalite:recordInfoLink/text()')
+    @element = element
+    create_from_paths
   end
 
-  def deleted?
-    return false
+  def xpath(path)
+    REXML::XPath.match(self.element, path)
   end
 
+  def create_from_paths
+    @title = xpath('cdwalite:cdwalite/cdwalite:titleWrap/cdwalite:titleSet/cdwalite:title[@cdwalite:pref="preferred"]/text()')
+    @creator = xpath('cdwalite:cdwalite/cdwalite:displayCreator/text()')
+    @date = xpath('cdwalite:cdwalite/cdwalite:indexingDatesWrap/cdwalite:indexingDatesSet/cdwalite:earliestDate/text()')
+    @materials = xpath('cdwalite:cdwalite/cdwalite:displayMaterialsTech/text()')
+    @description = xpath('cdwalite:cdwalite/cdwalite:descriptiveNoteWrap/cdwalite:descriptiveNoteSet/cdwalite:descriptiveNote/text()')
+    @subject = xpath('cdwalite:cdwalite/cdwalite:indexingSubjectWrap/cdwalite:indexingSubjectSet/cdwalite:subjectTerm[@cdwalite:termsource="WAC"]/text()')
+    @classification = xpath('cdwalite:cdwalite/cdwalite:classificationWrap/cdwalite:classification[@cdwalite:termsource="WAC"]/text()')
+    @image = xpath('cdwalite:cdwalite/cdwalite:resourceWrap/cdwalite:resourceSet/cdwalite:resourceType[contains(text(), "Digital Image")]/../cdwalite:linkResource/text()')
+    @related = xpath('cdwalite:cdwalite/cdwalite:resourceWrap/cdwalite:resourceSet/cdwalite:resourceType[contains(text(), "text")]/../cdwalite:resourceID/text()')
+    @url = xpath('cdwalite:cdwalite/cdwalite:recordWrap/cdwalite:recordInfoWrap/cdwalite:recordInfoLink/text()')
+    self
+  end
+
+
+  def as_json
+    hash = {}
+    attrs = instance_variables
+    attrs.delete(:@element)
+    attrs.each do |var|
+      key = var.to_s.gsub("@", "")
+      hashy.merge({key => send(key)})
+    end
+    hash
+  end
 end
